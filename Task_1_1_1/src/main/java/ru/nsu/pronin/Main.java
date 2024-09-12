fedc21da496473ae22f5027e45e1b480a91060bf
KolyaPronin <kolya.pronin.0506@mail.ru>
2024-09-12 20:25:13 +0700

    test1

--- Task_1_1_1/src/main/java/ru/nsu/pronin/Main.java
+++ Task_1_1_1/src/main/java/ru/nsu/pronin/Main.java
@@ -1,11 +1,127 @@
 package ru.nsu.pronin;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
-    public static int sum(int a, int b){
-        return a+b;
-    }
-    public static void main(String[] args){ System.out.println("Hello world");
-    }
+        public static class Heap {
+            int size;
+            int capacity;
+            int[] heap;
+
+            public Heap(int capacity){
+                this.size = 0;
+                this.capacity = capacity;
+                this.heap = new int[capacity];
+            }
+        }
+
+        static Heap createHeap(int capacity){
+            Heap temp = new Heap(capacity); // так память выделяется. Ключевое слово new
+            temp.size = 0;
+            temp.capacity = capacity;
+            return temp;
+        }
+
+        static void swap(Heap obj,int x,int y){
+            int temp = obj.heap[x];
+            obj.heap[x] = obj.heap[y];
+            obj.heap[y] = temp;
+        }
+
+        static int parent(int v){
+
+            return (v-1)/2;
+        }
+
+        static int left_child(Heap obj, int v){
+
+            if (obj.size < v * 2 + 1)
+                return -1;
+            return v*2 +1;
+        }
+
+        static int right_child(Heap obj, int v){
+            if (obj.size < v * 2 + 2)
+                return -1;
+            return v * 2 + 2;
+        }
+
+        static void siftUp(Heap obj, int v) {
+            if (v == 0) {
+                return;
+            }
+            if (obj.heap[v] < obj.heap[parent(v)]) {
+                swap(obj, v, parent(v));
+                siftUp(obj, parent(v));
+            }
+        }
+
+        static void siftDown(Heap obj, int ind) {
+            int leftChild = left_child(obj, ind);
+            int rightChild = right_child(obj, ind);
+            int min = ind;
+
+            if (leftChild != -1 && leftChild < obj.size && obj.heap[leftChild] < obj.heap[min]) {
+                min = leftChild;
+            }
+            if (rightChild != -1 && rightChild < obj.size && obj.heap[rightChild] < obj.heap[min]) {
+                min = rightChild;
+            }
+
+            if (min != ind) {
+                swap(obj, min, ind);
+                siftDown(obj, min);
+            }
+        }
+
+
+
+        static int extractMin(Heap obj) {
+            int answer = obj.heap[0];
+            obj.heap[0] = obj.heap[--obj.size];
+            siftDown(obj, 0);
+            return answer;
+        }
+
+        static void add(Heap obj, int value) {
+            obj.heap[obj.size] = value;
+            obj.size++;
+            siftUp(obj, obj.size - 1);
+        }
+
+        static void deleteHeap(Heap obj) {
+            obj.heap = null;
+        }
+
+        static int[] sortArray(int[] numbers) {
+            int n = numbers.length;
+            Heap tmp = createHeap(n);
+            for (int num : numbers) {
+                add(tmp, num);
+            }
+
+            int[] res = new int[n];
+            for (int i = 0; i < n; i++) {
+                res[i] = extractMin(tmp);
+            }
+
+            deleteHeap(tmp);
+            return res;
+        }
+        public static void main(String[] args) {
+
+            Scanner in = new Scanner(System.in);
+
+            int N = in.nextInt();
+            int[] array = new int[N];
+            for(int i = 0;i<N;i++){
+                array[i] = in.nextInt();
+            }
+            int[] sortedArray = sortArray(array);
+
+            for (int i = 0; i < N; i++) {
+                System.out.print(sortedArray[i] + " ");
+            }
+        }
+
 }
--- Task_1_1_1/src/test/java/ru/nsu/pronin/MainTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/pronin/MainTest.java
@@ -5,8 +5,20 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
+
     @Test
-    void SampleTest(){
-        assertEquals( 3, Main.sum(1,2) );
+    void Test_1() {
+        int[] input_array = {69, 87, 47, 106, 5, 46, 183, 2, 10};
+        int[] expected_array = {2, 5, 10, 46, 47, 69, 87, 106, 183};
+        int[] output_array = Main.sortArray(input_array);
+        assertArrayEquals(expected_array, output_array);
+    }
+
+    @Test
+    void Test_2() {
+        int[] input_array = {-100, -1231, 38, 103, 16, 1, 55, -6, -232, -99528, 308, 262, 232, 153, 5, -69};
+        int[] expected_array = {-99528, -1231, -232, -100, -69, -6, 1, 5, 16, 38, 55, 103, 153, 232, 262, 308};
+        int[] output_array = Main.sortArray(input_array);
+        assertArrayEquals(expected_array, output_array);
     }
 }
