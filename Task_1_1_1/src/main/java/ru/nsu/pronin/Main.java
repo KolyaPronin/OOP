995d5709377ca6deac48970513816a1a145d8877
KolyaPronin <kolya.pronin.0506@mail.ru>
2024-09-15 18:39:23 +0700

    edits made to checkstyle

--- Task_1_1_1/src/main/java/ru/nsu/pronin/Main.java
+++ Task_1_1_1/src/main/java/ru/nsu/pronin/Main.java
@@ -2,50 +2,94 @@ package ru.nsu.pronin;
 
 import java.util.Scanner;
 
+/**
+ * The main program class that implements array sorting using the heap.
+ */
+
 public class Main {
+        /**
+         * Heap data structure.
+         */
         public static class Heap {
             int size;
             int capacity;
             int[] heap;
-
+            /**
+             * Constructor for creating a heap object with a given capacity.
+             *
+             * @param capacity the initial capacity of the heap.
+             */
             public Heap(int capacity){
                 this.size = 0;
                 this.capacity = capacity;
                 this.heap = new int[capacity];
             }
         }
-
+        /**
+         * Creates a new heap with the given capacity.
+         *
+         * @param capacity the capacity of the heap to be created.
+         * @return the new heap object.
+         */
         static Heap createHeap(int capacity){
-            Heap temp = new Heap(capacity); // так память выделяется. Ключевое слово new
+            Heap temp = new Heap(capacity);
             temp.size = 0;
             temp.capacity = capacity;
             return temp;
         }
-
+        /**
+         * Swaps two heap elements.
+         *
+         * @param obj heap object.
+         * @param x index of the first element.
+         * @param y index of the second element.
+         */
         static void swap(Heap obj,int x,int y){
             int temp = obj.heap[x];
             obj.heap[x] = obj.heap[y];
             obj.heap[y] = temp;
         }
-
+        /**
+         * Вычисляет индекс родительского элемента для узла кучи.
+         *
+         * @param v индекс узла.
+         * @return индекс родителя.
+         */
         static int parent(int v){
 
             return (v-1)/2;
         }
-
+        /**
+         * Вычисляет индекс левого потомка узла в куче.
+         *
+         * @param obj объект кучи.
+         * @param v индекс узла.
+         * @return индекс левого потомка или -1, если потомок отсутствует.
+         */
         static int left_child(Heap obj, int v){
 
             if (obj.size < v * 2 + 1)
                 return -1;
             return v*2 +1;
         }
-
+        /**
+         * Вычисляет индекс правого потомка узла в куче.
+         *
+         * @param obj объект кучи.
+         * @param v индекс узла.
+         * @return индекс правого потомка или -1, если потомок отсутствует.
+         */
         static int right_child(Heap obj, int v){
             if (obj.size < v * 2 + 2)
                 return -1;
             return v * 2 + 2;
         }
-
+        /**
+         * Поднимает элемент вверх по куче для сохранения свойства минимальной кучи.
+         *
+         * @param obj объект кучи.
+         * @param v индекс элемента для подъема.
+         */
         static void siftUp(Heap obj, int v) {
             if (v == 0) {
                 return;
@@ -55,7 +99,12 @@ public class Main {
                 siftUp(obj, parent(v));
             }
         }
-
+        /**
+         * Опускает элемент вниз по куче для сохранения свойства минимальной кучи.
+         *
+         * @param obj объект кучи.
+         * @param ind индекс элемента для опускания.
+         */
         static void siftDown(Heap obj, int ind) {
             int leftChild = left_child(obj, ind);
             int rightChild = right_child(obj, ind);
@@ -75,24 +124,44 @@ public class Main {
         }
 
 
-
+        /**
+         * Retrieves the minimum element from the heap.
+         *
+         * @param obj heap object
+         * @return minimum element
+         */
         static int extractMin(Heap obj) {
             int answer = obj.heap[0];
             obj.heap[0] = obj.heap[--obj.size];
             siftDown(obj, 0);
             return answer;
         }
-
+        /**
+         * Adds an element to the heap.
+         *
+         * @param obj heap object
+         * @param value the value to add
+         */
         static void add(Heap obj, int value) {
             obj.heap[obj.size] = value;
             obj.size++;
             siftUp(obj, obj.size - 1);
         }
-
+        /**
+         * Deletes a heap.
+         *
+         * @param obj heap object
+         */
         static void deleteHeap(Heap obj) {
             obj.heap = null;
         }
 
+        /**
+         * Sorts an array of numbers using a heap data structure.
+         *
+         * @param numbers an array of numbers to sort
+         * @return sorted array of numbers
+         */
         static int[] sortArray(int[] numbers) {
             int n = numbers.length;
             Heap tmp = createHeap(n);
@@ -108,6 +177,12 @@ public class Main {
             deleteHeap(tmp);
             return res;
         }
+        /**
+         * The main method of the program.
+         * Performs reading an array of numbers, sorting the array and outputting the sorted numbers.
+         *
+         * @param args command line arguments (not used).
+         */
         public static void main(String[] args) {
 
             Scanner in = new Scanner(System.in);
--- Task_1_1_1/src/test/java/ru/nsu/pronin/MainTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/pronin/MainTest.java
@@ -2,23 +2,28 @@ package ru.nsu.pronin;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 
 class MainTest {
 
     @Test
-    void Test_1() {
-        int[] input_array = {69, 87, 47, 106, 5, 46, 183, 2, 10};
-        int[] expected_array = {2, 5, 10, 46, 47, 69, 87, 106, 183};
-        int[] output_array = Main.sortArray(input_array);
-        assertArrayEquals(expected_array, output_array);
+    void test1() {
+        int[] inputArray  = {69, 87, 47, 106, 5, 46, 183, 2, 10};
+        int[] expectedArray  = {2, 5, 10, 46, 47, 69, 87, 106, 183};
+        int[] outputArray  = Main.sortArray(inputArray);
+        assertArrayEquals(expectedArray, outputArray);
     }
 
     @Test
-    void Test_2() {
-        int[] input_array = {-100, -1231, 38, 103, 16, 1, 55, -6, -232, -99528, 308, 262, 232, 153, 5, -69};
-        int[] expected_array = {-99528, -1231, -232, -100, -69, -6, 1, 5, 16, 38, 55, 103, 153, 232, 262, 308};
-        int[] output_array = Main.sortArray(input_array);
-        assertArrayEquals(expected_array, output_array);
+    void test2() {
+        int[] inputArray = {
+                -100, -1231, 38, 103, 16, 1, 55, -6, -232, -99528, 308, 262, 232, 153, 5, -69
+        };
+        int[] expectedArray = {
+                -99528, -1231, -232, -100, -69, -6, 1, 5, 16, 38, 55, 103, 153, 232, 262, 308
+        };
+        int[] outputArray = Main.sortArray(inputArray);
+        assertArrayEquals(expectedArray, outputArray);
     }
 }
