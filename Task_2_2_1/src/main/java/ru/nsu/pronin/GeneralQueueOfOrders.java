806bbdeb87774074ba073cfa0425ae4b25fc0668
Nikolay Pronin <n.pronin@g.nsu.ru>
2025-03-10 19:09:06 +0700

    Task_2_2_1

--- Task_2_2_1/src/main/java/ru/nsu/pronin/GeneralQueueOfOrders.java
+++ Task_2_2_1/src/main/java/ru/nsu/pronin/GeneralQueueOfOrders.java
@@ -25,50 +25,52 @@ public class GeneralQueueOfOrders {
 /**
  * Класс OrderRealization реализует интерфейс Order, используя геттеры и сеттеры.
  * */
-class OrderRealization implements Order{
+class OrderRealization implements Order {
     private final int id;
     private final String type;
-    private String state ;
+    private String state;
 
     /**
      * метод setState заполняет поле state интерфейса Order
+     *
      * @param state - состояние заказа:
-     *             [заказан, готовится пекарем, готов, отправлен на склад, в пути, доставлен]
-     * */
-    public void setState(String state){
+     *              [заказан, готовится пекарем, готов, отправлен на склад, в пути, доставлен]
+     */
+    public void setState(String state) {
         this.state = state;
     }
 
     /**
      * метод getId возвращает id пекаря.
-     * */
-    public int getId(){
+     */
+    public int getId() {
         return id;
     }
 
     /**
      * метод getType возвращает type пекаря.
-     * */
-    public String getType(){ // doesn't used
+     */
+    public String getType() { // doesn't used
         return type;
     }
 
     /**
      * метод getState возвращает state пекаря.
-     * */
-    public String getState(){
+     */
+    public String getState() {
         return state;
     }
 
     /**
      * Конструктор класса OrderRealization который определяет поля.
-     * @param id - id повара
-     * @param type - type (тип заказа)
+     *
+     * @param id    - id повара
+     * @param type  - type (тип заказа)
      * @param state - state (состояние заказа)
-     * */
-    OrderRealization(int id, String type, String state){
+     */
+    OrderRealization(int id, String type, String state) {
         this.id = id;
         this.type = type;
         this.state = state;
     }
-
+}
