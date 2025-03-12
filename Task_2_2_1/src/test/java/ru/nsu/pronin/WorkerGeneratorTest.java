9462d7858034f929affd8f8dc4375ce5317ef409
Nikolay Pronin <n.pronin@g.nsu.ru>
2025-03-12 22:38:39 +0700

    Improved coverage

--- Task_2_2_1/src/test/java/ru/nsu/pronin/WorkerGeneratorTest.java
+++ Task_2_2_1/src/test/java/ru/nsu/pronin/WorkerGeneratorTest.java
@@ -8,7 +8,10 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import ru.nsu.pronin.data.Order;
 import ru.nsu.pronin.data.OrderImpl;
+import ru.nsu.pronin.people.BagMan;
+import ru.nsu.pronin.people.BagManWorker;
 import ru.nsu.pronin.people.Baker;
+import ru.nsu.pronin.people.BakerThread;
 import ru.nsu.pronin.process.GeneralQueueOfOrders;
 import ru.nsu.pronin.process.OrderGenerator;
 import ru.nsu.pronin.process.Warehouse;
@@ -35,5 +38,45 @@ class WorkerGeneratorTest {
         WorkerGenerator.workerBagManGenerator(2);
         Thread.sleep(10000);
         assertTrue(Warehouse.getQueueOfOrder().size() < 5);
+    }
+
+    @Test
+    void testWorkerBakerInterrupted() throws InterruptedException {
+        List<Order> orders = OrderGenerator.generateOrders(5);
+        GeneralQueueOfOrders generalQueueOfOrders = new GeneralQueueOfOrders();
+        generalQueueOfOrders.placeAllOrders(orders);
+
+        // Создаём и запускаем поток пекаря
+        Baker baker = new Baker(0,1000,generalQueueOfOrders);
+        BakerThread bakerThread = new BakerThread(baker);
+        bakerThread.start();
+
+        // Даем немного времени на выполнение
+        Thread.sleep(1000);
+
+        // Прерываем поток
+        bakerThread.interrupt();
+
+        // Проверяем, что исключение не выбрасывается за пределы
+        assertDoesNotThrow(() -> bakerThread.join());
+    }
+
+    @Test
+    void testBagManWorkerInterrupted() throws InterruptedException {
+        GeneralQueueOfOrders generalQueueOfOrders = new GeneralQueueOfOrders();
+        BagMan bagMan = new BagMan(0,1,1000);
+        BagManWorker bagManWorker = new BagManWorker(bagMan);
+
+        // Запускаем поток курьера
+        bagManWorker.start();
+
+        // Даем немного времени на выполнение
+        Thread.sleep(1000);
+
+        // Прерываем поток
+        bagManWorker.interrupt();
+
+        // Проверяем, что исключение не выбрасывается за пределы
+        assertDoesNotThrow(() -> bagManWorker.join());
     }
 }
