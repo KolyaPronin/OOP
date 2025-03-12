2b6a4c88e7001cd9945744d82243c5ba31eb90df
KolyaPronin <KolyaPronin@users.noreply.github.com>
2025-03-12 07:37:38 +0000

    Deploying to gh-pages from @ KolyaPronin/OOP@92b7050cf776decef737abb18620f43c82c42c54 🚀

--- Task_2_2_1/package-search-index.js
+++ Task_2_2_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.pronin"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.pronin"},{"l":"ru.nsu.pronin.data"},{"l":"ru.nsu.pronin.people"},{"l":"ru.nsu.pronin.process"}];updateSearchResults();
